6080710399efd589467ad5650e3402a35d325376
Cathy McDonald <github.cathy_mcdonald@spamgourmet.com>
2017-05-25 22:32:25 +1000

    Ignore empty strings in comma separated list

--- module1-assignment/js/app.js
+++ module1-assignment/js/app.js
@@ -12,21 +12,24 @@
 		$scope.colour = "white";
 		
 		$scope.checkLunch = function() {
-			if ($scope.list) {
-				$scope.colour = "green";
-				var lunchItems = $scope.list.split(",");
-				if (lunchItems.length > 3) {
-					$scope.message = "Too much!";
-				}
-				else { 
-					$scope.message = "Enjoy!";
-				}
-			}
-			else {
+			var lunchItems = $scope.list.split(",");
+			// Filter out empty strings
+			lunchItems = lunchItems.filter(function(item) { return item.trim() != ''; });
+			
+			if (lunchItems.length == 0) {
 				$scope.colour = "red";
 				$scope.message = "Please enter data first";
 			}
-		}
+			else if (lunchItems.length > 3) {
+				$scope.colour = "green";
+				$scope.message = "Too much!";
+			}
+			else { 
+				$scope.colour = "green";
+				$scope.message = "Enjoy!";
+			}
+		}		
 	}
+	
 
 })();
