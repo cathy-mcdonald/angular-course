5054677ee76efd29f7496903beda225cc64a332e
Cathy McDonald <github.cathy_mcdonald@spamgourmet.com>
2017-05-25 22:06:19 +1000

    Add colour to messages

--- module1-assignment/js/app.js
+++ module1-assignment/js/app.js
@@ -9,9 +9,11 @@
 	function LunchCheckController($scope) {
 		$scope.list = "";
 		$scope.message = "";
+		$scope.colour = "white";
 		
 		$scope.checkLunch = function() {
 			if ($scope.list) {
+				$scope.colour = "green";
 				var lunchItems = $scope.list.split(",");
 				if (lunchItems.length > 3) {
 					$scope.message = "Too much!";
@@ -21,7 +23,8 @@
 				}
 			}
 			else {
-				$scope.message = "Please enter data first"
+				$scope.colour = "red";
+				$scope.message = "Please enter data first";
 			}
 		}
 	}
