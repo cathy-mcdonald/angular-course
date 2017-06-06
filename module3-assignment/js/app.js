367cb1bd10f747fe9d1b740b97c23baa8148bb18
Cathy McDonald <github.cathy_mcdonald@spamgourmet.com>
2017-06-06 20:14:16 +1000

    Add http request and search functionality

--- module3-assignment/js/app.js
+++ module3-assignment/js/app.js
@@ -1,18 +1,53 @@
 (function() {
 	'use strict';
 
-	angular.module('NarrowItDownApp', [])
-	.controller('NarrowItDownController', NarrowItDownController)
-	.service('MenuSearchService', MenuSearchService);
-	
-	NarrowItDownController.$inject = ['MenuSearchService'];
+	angular.module('NarrowItDownApp', []).controller('NarrowItDownController',
+			NarrowItDownController).service('MenuSearchService',
+			MenuSearchService);
+
+	NarrowItDownController.$inject = [ 'MenuSearchService' ];
 	function NarrowItDownController(MenuSearchService) {
 		var narrowIt = this;
 		
-	}
-	
-	function MenuSearchService() {
+		narrowIt.searchTerm = '';
 		
+		narrowIt.search = function() {
+			
+			var promise = MenuSearchService.getMatchedMenuItems(narrowIt.searchTerm.toLowerCase());
+			
+			promise.then(function (response) {
+				console.log(response)
+				narrowIt.found = response;				
+		    })
+		    .catch(function (error) {
+		      console.log(error);
+		    });
+		}
+
 	}
-	
+
+	MenuSearchService.$inject = ['$http']
+	function MenuSearchService($http) {
+		var service = this;
+
+		service.getMatchedMenuItems = function(searchTerm) {
+			return $http({
+				method : 'GET',
+				url : 'https://davids-restaurant.herokuapp.com/menu_items.json'
+			}).then(function(result) {				
+				var menuItems = result.data.menu_items;
+				var foundItems = [];
+				
+				// process result and only keep items that match
+				for (var i = 0; i < menuItems.length; i++) {
+					if (menuItems[i].description.indexOf(searchTerm) !== -1) {
+						foundItems.push(menuItems[i]);
+					}
+				}
+
+				return foundItems;
+			});
+		}
+	}
+
 })();
