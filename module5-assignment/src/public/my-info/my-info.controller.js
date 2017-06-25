048688ea94c166fc3b95a2bbffc741aa79b4b3f6
Cathy McDonald <github.cathy_mcdonald@spamgourmet.com>
2017-06-25 18:33:53 +1000

    Add menu item details to MyInfo view

--- module5-assignment/src/public/my-info/my-info.controller.js
+++ module5-assignment/src/public/my-info/my-info.controller.js
@@ -4,10 +4,14 @@
 angular.module('public')
 .controller('MyInfoController', MyInfoController);
 
-MyInfoController.$inject = ['SignupService'];
-function MyInfoController(SignupService) {
+MyInfoController.$inject = ['SignupService', 'MenuService'];
+function MyInfoController(SignupService, MenuService) {
   var $ctrl = this;
   $ctrl.details = SignupService.retrieveSignup();
+  MenuService.getMenuItem($ctrl.details.favouriteDish)
+  .then(function(menuItem) {
+  	$ctrl.details.menuItem = menuItem;
+  });
 }
 
 })();
